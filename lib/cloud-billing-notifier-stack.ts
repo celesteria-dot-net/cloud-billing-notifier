6112b370b152c3fd59a5c9d3581a5e62835015c5
Lucky <lucky3028@users.noreply.github.com>
2021-04-17 21:14:30 +0900

    [Add] スタックの記述

--- lib/cloud-billing-notifier-stack.ts
+++ lib/cloud-billing-notifier-stack.ts
@@ -1,9 +1,34 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from "@aws-cdk/core";
+import * as lambda from "@aws-cdk/aws-lambda";
+import { Duration } from "@aws-cdk/core";
+import { NODE_LAMBDA_LAYER_DIR } from "./process/setup";
+import ENVIRONMENTS from "../lambda/utils/env";
 
 export class CloudBillingNotifierStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const nodeModulesLayer = new lambda.LayerVersion(this, "NodeModulesLayer", {
+      code: lambda.AssetCode.fromAsset(NODE_LAMBDA_LAYER_DIR),
+      compatibleRuntimes: [lambda.Runtime.NODEJS_12_X],
+    });
+
+    const lambdaFunc = new lambda.Function(this, "aws-billing", {
+      runtime: lambda.Runtime.NODEJS_12_X,
+      code: lambda.Code.fromAsset("out/lambda"),
+      handler: "index.handler",
+      environment: {
+        EXCHANGE_RATE_SCRAPING_ACCOUNT_ID:
+          ENVIRONMENTS.EXCHANGE_RATE_SCRAPING_ACCOUNT_ID,
+        EXCHANGE_RATE_SCRAPING_USERNAME:
+          ENVIRONMENTS.EXCHANGE_RATE_SCRAPING_USERNAME,
+        EXCHANGE_RATE_SCRAPING_PASSWORD:
+          ENVIRONMENTS.EXCHANGE_RATE_SCRAPING_PASSWORD,
+        DISCORD_WEBHOOK: ENVIRONMENTS.DISCORD_WEBHOOK,
+      },
+      timeout: Duration.minutes(1),
+      memorySize: 512,
+      layers: [nodeModulesLayer],
+    });
   }
 }
