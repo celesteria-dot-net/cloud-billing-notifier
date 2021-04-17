52ef3f9e6b366733f0ed1ee628bbe95358a3fad8
Lucky <lucky3028@users.noreply.github.com>
2021-04-17 21:55:06 +0900

    [Add] puppeteerのブラウザオプションをまとめる

---
+++ lambda/utils/puppeteer.ts
@@ -0,0 +1,13 @@
+const BROWSER_OPTIONS = {
+  headless: true,
+  defaultViewport: { width: 1920, height: 1080 },
+  args: [
+    "--single-process",
+    "--disable-gpu ",
+    "--no-sandbox",
+    "--disable-setuid-sandbox",
+    "--disable-dev-shm-usage",
+  ],
+}
+
+export default BROWSER_OPTIONS
