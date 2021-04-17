1e3a46822d9668689ccac3ff5091431550b71a4e
Lucky <lucky3028@users.noreply.github.com>
2021-04-17 21:13:19 +0900

    [Add] envを読み込むファイル

---
+++ lambda/utils/env.ts
@@ -0,0 +1,10 @@
+import { load } from "ts-dotenv";
+
+const ENVIRONMENTS = load({
+  EXCHANGE_RATE_SCRAPING_ACCOUNT_ID: String,
+  EXCHANGE_RATE_SCRAPING_USERNAME: String,
+  EXCHANGE_RATE_SCRAPING_PASSWORD: String,
+  DISCORD_WEBHOOK: String,
+});
+
+export default ENVIRONMENTS;
