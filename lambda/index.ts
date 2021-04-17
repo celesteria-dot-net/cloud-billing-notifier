4f16208062bcc441bc3fdc65cf7356824b64e880
Lucky <lucky3028@users.noreply.github.com>
2021-04-17 22:44:35 +0900

    [Add] discordへの通知

--- lambda/index.ts
+++ lambda/index.ts
@@ -1,6 +1,8 @@
 import chromium from "chrome-aws-lambda";
 import { loginAws, fetchExchangeRate, fetchMonthSum } from "./domains/aws";
 import BROWSER_OPTIONS from "./utils/puppeteer";
+import webhook from "./domains/discord";
+import { MessageBuilder } from "discord-webhook-node";
 
 export const handler = async () => {
   const browser = await chromium.puppeteer.launch({
@@ -20,8 +22,15 @@ export const handler = async () => {
 
   await browser.close();
 
+  const embed = new MessageBuilder()
+    .setAuthor("AWS Cost Information")
+    .setDescription(`${new Date().getFullYear()}/${new Date().getMonth()}分の現時点での使用量通知です。`)
+    .addField("今月の請求(JPY)", monthlySum.toString(), true)
+    .addField("為替レート(JPY)", rate.toString(), true);
+  await webhook.send(embed);
+
   return {
     rate,
-    monthlySum
+    monthlySum,
   };
 };
