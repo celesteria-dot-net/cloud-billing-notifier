5331cb3bfb33473d60b7063a3f47535c6c467996
Lucky <lucky3028@users.noreply.github.com>
2021-04-18 14:27:52 +0900

    [Change] Discordに送信する数字を四捨五入

--- lambda/index.ts
+++ lambda/index.ts
@@ -3,6 +3,7 @@ import { loginAws, fetchExchangeRate, fetchMonthSum } from "./domains/aws";
 import BROWSER_OPTIONS from "./utils/puppeteer";
 import webhook from "./domains/discord";
 import { MessageBuilder } from "discord-webhook-node";
+import round from "./utils/date";
 
 export const handler = async () => {
   const browser = await chromium.puppeteer.launch({
@@ -24,9 +25,9 @@ export const handler = async () => {
 
   const embed = new MessageBuilder()
     .setAuthor("AWS Cost Information")
-    .setDescription(`${new Date().getFullYear()}/${new Date().getMonth() + 1}分の現時点での使用量通知です。`)
-    .addField("今月の請求(JPY)", monthlySum.toString(), true)
-    .addField("為替レート(JPY)", rate.toString(), true);
+    .setDescription(`${new Date().getFullYear()}/${new Date().getMonth() + 1}における現時点での請求額通知です。`)
+    .addField("今月の請求(JPY)", round(monthlySum, 100).toString(), true)
+    .addField("為替レート(JPY)", round(rate, 100).toString(), true);
   await webhook.send(embed);
 
   return {
