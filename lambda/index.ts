832bf42361e2c4b7c91e9271b295a06d89969243
Lucky <lucky3028@users.noreply.github.com>
2021-04-18 15:23:48 +0900

    [Fix] importの順番を変更

--- lambda/index.ts
+++ lambda/index.ts
@@ -1,9 +1,16 @@
 import chromium from "chrome-aws-lambda";
-import { loginAws, fetchExchangeRate, fetchMonthSum, billingScrShotPath, takeScrShot, moveToBillingHome } from "./domains/aws";
-import BROWSER_OPTIONS from "./utils/puppeteer";
-import webhook from "./domains/discord";
 import { MessageBuilder } from "discord-webhook-node";
-import { format } from 'date-fns'
+import { format } from "date-fns";
+import {
+  loginAws,
+  fetchExchangeRate,
+  fetchMonthSum,
+  billingScrShotPath,
+  takeScrShot,
+  moveToBillingHome,
+} from "./domains/aws";
+import webhook from "./domains/discord";
+import BROWSER_OPTIONS from "./utils/puppeteer";
 import round from "./utils/number";
 
 export const handler = async () => {
@@ -28,7 +35,9 @@ export const handler = async () => {
   const monthlySum = await fetchMonthSum(page);
 
   await takeScrShot(page).catch((err) => {
-    console.error("サービス別請求内訳のスクリーンショットの作成に失敗しました。");
+    console.error(
+      "サービス別請求内訳のスクリーンショットの作成に失敗しました。"
+    );
     console.error(err);
   });
 
@@ -36,11 +45,16 @@ export const handler = async () => {
 
   const embed = new MessageBuilder()
     .setAuthor("AWS Cost Information")
-    .setDescription(`${format(new Date(), 'yyyy/MM')}における現時点での請求額通知です。`)
+    .setDescription(
+      `${format(new Date(), "yyyy/MM")}における現時点での請求額通知です。`
+    )
     .addField("今月の請求(JPY)", round(monthlySum, 100).toString(), true)
     .addField("為替レート(JPY)", round(rate, 100).toString(), true)
     .setImage(billingScrShotPath);
-  await webhook.send(embed);
+  await webhook.send(embed).catch((err) => {
+    console.error("DiscordにWebhookを送信できませんでした。");
+    console.error(err);
+  });
 
   return {
     rate,
